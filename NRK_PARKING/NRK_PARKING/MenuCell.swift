a66c57e0ee441ce68edc8ffbc01cc33b822d2fdb
khushboo <khushboobhatoia@gmail.com>
2018-03-02 12:46:42 -0500

    Add files via upload

---
+++ NRK_PARKING/NRK_PARKING/MenuCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuCell.swift
+//  NRK_PARKING
+//
+//  Created by MacStudent on 2018-03-02.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UITableViewCell {
+
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
