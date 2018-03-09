2b0d04f1b5728643dcf91c876db91076eca67883
khushboo <khushboobhatoia@gmail.com>
2018-03-09 11:30:41 -0500

    Add files via upload

---
+++ DAY12_PAGE_BASED_APP/DAY12_PAGE_BASED_APP/ClassPVC.swift
@@ -0,0 +1,102 @@
+//
+//  ClassPVC.swift
+//  DAY12_PAGE_BASED_APP
+//
+//  Created by MacStudent on 2018-03-09.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ClassPVC: UIPageViewController
+{
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newColoredViewController(color: "Green"),
+                self.newColoredViewController(color: "Red"),
+                self.newColoredViewController(color: "Blue")]
+    }()
+    
+    private func newColoredViewController(color: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewController(withIdentifier: "\(color)ViewController")
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        dataSource = self as? UIPageViewControllerDataSource
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+    }
+
+        func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+}
+}
+
+extension ClassPVC: UIPageViewControllerDataSource
+{
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        // User is on the first view controller and swiped left to loop to
+        // the last view controller.
+        guard previousIndex >= 0 else {
+            return orderedViewControllers.last
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        // User is on the last view controller and swiped right to loop to
+        // the first view controller.
+        guard orderedViewControllersCount != nextIndex else {
+            return orderedViewControllers.first
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return orderedViewControllers.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        guard let firstViewController = viewControllers?.first,
+            let firstViewControllerIndex = orderedViewControllers.index(of: firstViewController) else {
+                return 0
+        }
+        
+        return firstViewControllerIndex
+    }
+    
+}
